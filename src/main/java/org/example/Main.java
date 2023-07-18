3773e46c8fe6e68b62cd68257ab17e03680a2366
JeremiePlanta <Jeremie.Planta@stahl-holding-saar.de>
2023-07-18 10:45:59 +0200

    Erstellen der Stammdaten in den DomainService StammdatenService ausgelagert, damit dies nicht mehr in der Main gemacht wird.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,9 @@ package org.example;
 import io.jexxa.core.JexxaMain;
 import io.jexxa.drivingadapter.rest.RESTfulRPCAdapter;
 import org.example.domain.Flugzeug;
+import org.example.domain.FlugzeugRepository;
 import org.example.domain.FlugzeugVerwaltung;
+import org.example.domainservice.StammdatenService;
 import org.example.infrastructure.drivenadapter.persistence.FlugzeugRepositoryImpl;
 
 import java.util.List;
@@ -18,22 +20,17 @@ public class Main {
 
     @SuppressWarnings("java:S106")
     public static void main(String[] args) {
-        FlugzeugVerwaltung flugzeugVerwaltung = new FlugzeugVerwaltung(new FlugzeugRepositoryImpl());
-        Flugzeug flugzeug747 =new Flugzeug(4,200,"Boeing 747");
-        Flugzeug flugzeug737 =new Flugzeug(120,2000,"Boeing 737");
-        flugzeugVerwaltung.add(flugzeug737);
-        flugzeugVerwaltung.add(flugzeug747);
-        List<Flugzeug> alleFlugzeuge = flugzeugVerwaltung.get();
-        alleFlugzeuge.forEach(flugzeug -> System.out.println(flugzeug.getModell()));
-        flugzeugVerwaltung.delete(flugzeug747);
-        alleFlugzeuge = flugzeugVerwaltung.get();
-        alleFlugzeuge.forEach(flugzeug -> System.out.println(flugzeug.getModell()));
+        FlugzeugRepository flugzeugRepository = new FlugzeugRepositoryImpl();
+        FlugzeugVerwaltung flugzeugVerwaltung = new FlugzeugVerwaltung(flugzeugRepository);
+        StammdatenService stammdatenService = new StammdatenService(flugzeugRepository);
+        stammdatenService.initStammdaten();
+
         var jexxaMain = new JexxaMain(Main.class);
 
 
         jexxaMain
 
-                .bind(RESTfulRPCAdapter.class).to(FlugzeugVerwaltung.class)
+                .bind(RESTfulRPCAdapter.class).to(flugzeugVerwaltung)
                 .bind(RESTfulRPCAdapter.class).to(Main.class)
                 .bind(RESTfulRPCAdapter.class).to(jexxaMain.getBoundedContext())
                 .run();
---
+++ src/main/java/org/example/domainservice/StammdatenService.java
@@ -0,0 +1,31 @@
+package org.example.domainservice;
+
+import org.example.domain.Flugzeug;
+import org.example.domain.FlugzeugRepository;
+
+import java.util.List;
+
+public class StammdatenService {
+
+    private static final Flugzeug boeing747 =new Flugzeug(4,200,"Boeing 747");
+    private static final Flugzeug boeing737 =new Flugzeug(120,2000,"Boeing 737");
+
+    private final FlugzeugRepository flugzeugRepository;
+
+    public StammdatenService(FlugzeugRepository flugzeugRepository) {
+        this.flugzeugRepository = flugzeugRepository;
+    }
+    public void initStammdaten(){
+
+        List<Flugzeug> alleFlugzeuge = flugzeugRepository.getAll();
+        if(!alleFlugzeuge.contains(boeing747)){
+            flugzeugRepository.add(boeing737);
+        }
+        if(!alleFlugzeuge.contains(boeing747)){
+            flugzeugRepository.add(boeing737);
+        }
+        flugzeugRepository
+                .getAll()
+                .forEach( element -> System.out.println(element.getModell()));
+    }
+}
